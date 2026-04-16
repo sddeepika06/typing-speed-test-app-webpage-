60accd206785f666ee1a749e52d4d69da3eb46cc
sddeepika06 <sddeepika06@gmail.com>
2026-04-17 00:07:23 +0530

    added some feature in leadership board

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const leaderboardRoutes = require('./routes/leaderboard');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
   credentials: true
 }));
 app.use(express.json());
